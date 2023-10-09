024db549f8ff545c4b549823c8d1ebf6224270ab
Ferid Memed <ferid.dragon@hotmail.com>
2023-10-09 15:20:42 +0200

    Fixed being able to create spell slots with 0 charges.

--- androidApp/src/main/java/com/feko/generictabletoprpg/tracker/SpellSlot.kt
+++ androidApp/src/main/java/com/feko/generictabletoprpg/tracker/SpellSlot.kt
@@ -7,6 +7,8 @@ class SpellSlot(var level: Int, id: Long, name: String, amount: Int, index: Int,
 
     override fun validate(): Boolean = super.validate() && isLevelValid()
 
+    override fun isValueValid(): Boolean = super.isValueValid() && amount > 0
+
     override fun copy(): TrackedThing =
         SpellSlot(level, id, name, amount, index, groupId).also { it.defaultValue = defaultValue }
 }
