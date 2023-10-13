dc5cb65ada71444dbf01658d000c1bc1edf92f9b
Ferid Memed <ferid.dragon@hotmail.com>
2023-10-13 12:52:20 +0200

    Added hint label to import button.

--- androidApp/src/main/java/com/feko/generictabletoprpg/import/Import.kt
+++ androidApp/src/main/java/com/feko/generictabletoprpg/import/Import.kt
@@ -3,7 +3,9 @@ package com.feko.generictabletoprpg.import
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -11,6 +13,8 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -75,19 +79,26 @@ fun ImportScreen(
         }
         when (screenState) {
             is ImportViewModel.ImportScreenState.ReadyToImport -> {
-                FloatingActionButton(
-                    onClick = {
-                        pickFileLauncher.launch("*/*")
-                    }
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Icon(Icons.Default.Add, "")
+                    FloatingActionButton(
+                        onClick = {
+                            pickFileLauncher.launch("*/*")
+                        }
+                    ) {
+                        Icon(Icons.Default.Add, "")
+                    }
+                    Text(
+                        stringResource(R.string.file_import_hint),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                    )
                 }
             }
 
             is ImportViewModel.ImportScreenState.Importing -> {
-                Box(
-                    Modifier.fillMaxSize()
-                ) {
+                Box(Modifier.fillMaxSize()) {
                     CircularProgressIndicator(
                         Modifier
                             .size(100.dp)
