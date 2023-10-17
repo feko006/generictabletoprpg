34b7538d1b9dbaa1ed72d7791a5ebe6388aea291
Ferid <ferid.dragon@hotmail.com>
2023-10-17 21:42:07 +0200

    Fixed ordering issue when using a capitalized search string.

--- androidApp/src/main/java/com/feko/generictabletoprpg/common/SmartNamedSearchComparator.kt
+++ androidApp/src/main/java/com/feko/generictabletoprpg/common/SmartNamedSearchComparator.kt
@@ -6,8 +6,8 @@ class SmartNamedSearchComparator<T>(private val searchString: String) : Comparat
     override fun compare(o1: T?, o2: T?): Int {
         val o1LowercaseName = (o1 as INamed).name.lowercase()
         val o2LowercaseName = (o2 as INamed).name.lowercase()
-        val o1StartsWithSearchString = o1LowercaseName.startsWith(searchString)
-        val o2StartsWithSearchString = o2LowercaseName.startsWith(searchString)
+        val o1StartsWithSearchString = o1LowercaseName.startsWith(searchString.lowercase())
+        val o2StartsWithSearchString = o2LowercaseName.startsWith(searchString.lowercase())
         val o1IsTrackedThing = o1 is TrackedThing
         val o2IsTrackedThing = o2 is TrackedThing
         return if (o1IsTrackedThing xor o2IsTrackedThing) {
