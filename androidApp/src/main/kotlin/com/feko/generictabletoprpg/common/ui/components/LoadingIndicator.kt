d33ebb3238f4a97e5bdf01c7561fb7434f399a3c
Ferid <ferid.dragon@hotmail.com>
2025-06-21 18:32:21 +0200

    Changed shapes of loading indicator to match dice.

--- androidApp/src/main/kotlin/com/feko/generictabletoprpg/common/ui/components/LoadingIndicator.kt
+++ androidApp/src/main/kotlin/com/feko/generictabletoprpg/common/ui/components/LoadingIndicator.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialShapes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +15,16 @@ import com.feko.generictabletoprpg.common.ui.theme.LocalDimens
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 fun FillingLoadingIndicator() {
     Box(Modifier.fillMaxSize(), Alignment.Center) {
-        LoadingIndicator(Modifier.size(LocalDimens.current.iconLarge))
+        LoadingIndicator(
+            Modifier.size(LocalDimens.current.iconLarge),
+            polygons = listOf(
+                MaterialShapes.Square,
+                MaterialShapes.Triangle,
+                MaterialShapes.Diamond,
+                MaterialShapes.Gem,
+                MaterialShapes.Pentagon,
+            ).shuffled()
+        )
     }
 }
 
