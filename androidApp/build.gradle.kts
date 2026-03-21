1e3e0fb55f840be8917bb835e90aa7d2443fdfb4
feko006 <ferid.memed6@gmail.com>
2026-03-21 21:13:19 +0100

    Updated version code and name.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -21,7 +21,7 @@ configure<ApplicationExtension> {
         applicationId = "com.feko.generictabletoprpg"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = libs.versions.app.get()
 
         vectorDrawables {
